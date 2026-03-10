8a974f0d7b3f7780716678dbc88995788b813bb5
Gobinda Chandra Sarker <gcsarker01@gmail.com>
2026-03-10 13:41:46 -0400

    test commit

---
+++ download.ps1
@@ -0,0 +1,21 @@
+$bucket="oedi-data-lake"
+
+$prefix="nrel-pds-building-stock/end-use-load-profiles-for-us-building-stock/2022/resstock_tmy3_release_1.1/timeseries_individual_buildings/by_state/upgrade=0/state=NY/"
+
+$files = aws s3api list-objects-v2 `
+--bucket $bucket `
+--prefix $prefix `
+--no-sign-request `
+--query "Contents[].Key" `
+--output text
+
+$offset = 0
+$n = 100
+
+$subset = $files.Split("`t")[$offset..($offset+$n-1)]
+
+foreach ($f in $subset) {
+    aws s3 cp --no-sign-request `
+    "s3://$bucket/$f" `
+    ".\dataset\$(Split-Path $f -Leaf)"
+}
